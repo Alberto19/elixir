e718b325091c1b104640327fd0ed60e010925248
Junior <junior@beetech.global>
2017-07-12 10:49:24 -0300

    Add contains method

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,17 +1,23 @@
 defmodule Cards do
   
-  @doc "Hello method"
+  @doc "return string Hello"
   def hello do 
     "Hello!"
   end
 
-  @doc "Array method"
+  @doc "return Array"
   def create_deck do 
-    ["Ace", "Two", "Three"]
+    ["Ace", "Two", "Three", "Four", "Five"]
   end
 
   @doc "Shuffle use the enum, your function shuffle is equal a Ramdom"
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
+
+  @doc "Check if contains value in enumerable"
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
 end
--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -2,7 +2,17 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "should return string hello" do 
+    assert Cards.hello == "Hello!"
   end
+
+  test "should return Array" do
+    assert Cards.create_deck === ["Ace", "Two", "Three"]
+  end
+
+  test "should check if value exist in enumerable" do
+    deck = ["Ace", "Two", "Three"]
+    assert Cards.contains?(deck, "Two") === true
+  end
+
 end
