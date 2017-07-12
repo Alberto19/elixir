0c060c1c3bf65cf08296557330a87c82c974430d
Junior <junior@beetech.global>
2017-07-12 10:20:41 -0300

    Add shuffle method - Enum

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,18 +1,17 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for Cards.
-  """
+  
+  @doc "Hello method"
+  def hello do 
+    "Hello!"
+  end
 
-  @doc """
-  Hello world.
+  @doc "Array method"
+  def create_deck do 
+    ["Ace", "Two", "Three"]
+  end
 
-  ## Examples
-
-      iex> Cards.hello
-      :world
-
-  """
-  def hello do
-    :world
+  @doc "Shuffle use the enum, your function shuffle is equal a Ramdom"
+  def shuffle(deck) do
+    Enum.shuffle(deck)
   end
 end
