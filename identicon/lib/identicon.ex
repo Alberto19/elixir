7bdef35d2d0c632cabb5e6337f83bbb41e88d5e2
Junior <junior@beetech.global>
2017-07-13 14:47:45 -0300

    Add method to create hash image

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -12,7 +12,34 @@ defmodule Identicon do
       :world
 
   """
-  def hello do
-    :world
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
   end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+  
+  def build_grid(%Identicon.Image{hex: hex} =image) do
+    hex
+    |> Enum.chunk(3)
+  end
+
+  def mirrow_row(row) do
+    # [145, 46, 200]
+    [first, second | _tail] = row  
+
+    # return [145, 46, 200, 46, 145]
+    row ++ [second, first]
+  end
+
 end
